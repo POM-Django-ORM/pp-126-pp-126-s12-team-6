1d54f263e1e993e9cca431cd6d7a31f49c92f675
Artem Snisarenko <artyom.snisarenko@gmail.com>
2025-03-26 21:37:06 +0200

    add book/models and add order/apps and models

--- library/order/models.py
+++ library/order/models.py
@@ -1,89 +1,84 @@
 from django.db import models
+from authentication.models import CustomUser
+from book.models import Book
 
+def format_dt(dt):
+    s = dt.strftime("%Y-%m-%d %H:%M:%S%z")
+    return s[:-2] + ":" + s[-2:] if len(s) >= 5 else s
 
 class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    end_at = models.DateTimeField(null=True, blank=True)
+    plated_end_at = models.DateTimeField()
 
-    def __str__(self):
-        """
-        Magic method is redefined to show all information about Order.
-        :return: book id, book name, book description, book count, book authors
-        """
+    @classmethod
+    def create(cls, user, book, plated_end_at):
+        if not user.pk:
+            return None
+        if book.count <= 1:
+            return None
+        try:
+            order = cls(user=user, book=book, plated_end_at=plated_end_at)
+            order.save()
+            book.count -= 1
+            book.save()
+            return order
+        except Exception:
+            return None
 
-    def __repr__(self):
-        """
-        This magic method is redefined to show class and id of Order object.
-        :return: class, id
-        """
-        return f'{self.__class__.__name__}(id={self.id})'
+    @classmethod
+    def delete_by_id(cls, order_id):
+        try:
+            order = cls.objects.get(id=order_id)
+            order.delete()
+            return True
+        except cls.DoesNotExist:
+            return False
+
+    @classmethod
+    def get_all(cls):
+        return list(cls.objects.all())
+
+    @classmethod
+    def get_by_id(cls, order_id):
+        try:
+            return cls.objects.get(id=order_id)
+        except cls.DoesNotExist:
+            return None
+
+    @classmethod
+    def get_not_returned_books(cls):
+        return list(cls.objects.filter(end_at__isnull=True))
+
+    def update(self, **kwargs):
+        allowed = ['end_at', 'plated_end_at']
+        for key, value in kwargs.items():
+            if key in allowed:
+                setattr(self, key, value)
+        self.save()
+        return self
 
     def to_dict(self):
-        """
-        :return: dict contains order id, book id, user id, order created_at, order end_at, order plated_end_at
-        :Example:
-        | {
-        |   'id': 8,
-        |   'book': 8,
-        |   'user': 8',
-        |   'created_at': 1509393504,
-        |   'end_at': 1509393504,
-        |   'plated_end_at': 1509402866,
-        | }
-        """
-        pass
+        dt_format = lambda dt: format_dt(dt) if dt else None
+        return {
+            'id': self.id,
+            'user': self.user.__repr__(),
+            'book': self.book.__repr__(),
+            'end_at': dt_format(self.end_at),
+            'plated_end_at': dt_format(self.plated_end_at),
+        }
 
-    @staticmethod
-    def create(user, book, plated_end_at):
-        """
-        :param user: the user who took the book
-        :type user: CustomUser
-        :param book: the book they took
-        :type book: Book
-        :param plated_end_at: planned return of data
-        :type plated_end_at: int (timestamp)
-        :return: a new order object which is also written into the DB
-        """
+    def __str__(self):
+        d = self.to_dict()
+        parts = []
+        for k in ['id', 'user', 'book', 'created_at', 'end_at', 'plated_end_at']:
+            if k in ['user', 'book']:
+                parts.append(f"'{k}': {d[k]}")
+            else:
+                parts.append(f"'{k}': {repr(d[k])}")
+        return ", ".join(parts)
 
-        pass
-
-    @staticmethod
-    def get_by_id(order_id):
-        """
-        :param order_id:
-        :type order_id: int
-        :return:  the object of the order, according to the specified id or null in case of its absence
-        """
-        pass
-
-    def update(self, plated_end_at=None, end_at=None):
-        """
-        Updates order in the database with the specified parameters.\n
-        :param plated_end_at: new plated_end_at
-        :type plated_end_at: int (timestamp)
-        :param end_at: new end_at
-        :type plated_end_at: int (timestamp)
-        :return: None
-        """
-        pass
-
-    @staticmethod
-    def get_all():
-        """
-        :return: all orders
-        """
-        pass
-
-    @staticmethod
-    def get_not_returned_books():
-        """
-        :return:  all orders that do not have a return date (end_at)
-        """
-        pass
-
-    @staticmethod
-    def delete_by_id(order_id):
-        """
-        :param order_id: an id of a user to be deleted
-        :type order_id: int
-        :return: True if object existed in the db and was removed or False if it didn't exist
-        """
-        pass
+    def __repr__(self):
+        return f"Order(id={self.id})"
